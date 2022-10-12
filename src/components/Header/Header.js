9f79dadcc6e6dd54313052bbf6cdeffe96af7ec9
ahm-rubayed <rubayedahmed04@gmail.com>
2022-10-12 07:35:21 +0600

    Header added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Header = () => {
     return (
-        
+        <div>
+            <h2 className='mt-5'>Welcome to the really awesome quizhoo for quiz experience...</h2>
+            <p className='fs-5'>Just answer a few questions and we'll provide you with not one. </p>
+        </div>
     );
 };
 
